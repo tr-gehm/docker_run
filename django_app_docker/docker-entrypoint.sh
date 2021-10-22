edfb5528cd00ccf779bb7362a177f90d6d7407a0
gehongming <1010562639@qq.com>
2021-10-22 14:24:49 +0800

    注释django数据库操作

--- django_app_docker/docker-entrypoint.sh
+++ django_app_docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-python manage.py makemigrations
-python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('user1', 'user1@qq.com', '123456')" | python manage.py shell &> /dev/null
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('user2', 'user2@qq.com', '123456')" | python manage.py shell &> /dev/null
+#python manage.py makemigrations
+#python manage.py migrate
+#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('user1', 'user1@qq.com', '123456')" | python manage.py shell &> /dev/null
+#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('user2', 'user2@qq.com', '123456')" | python manage.py shell &> /dev/null
 /usr/local/bin/gunicorn -c /usr/src/app/configs/gunicorn_config.py ${PROJECT_NAME}.wsgi
